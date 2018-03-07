2788a342e159324c81b3c2f37f75904217b708b9
DivyanshuD <divyanshu.dadheech@metacube.com>
2018-03-07 16:20:53 +0530

    added comments for grading_scale_steps_controller

--- app/controllers/api/v1/grading_scale_steps_controller.rb
+++ app/controllers/api/v1/grading_scale_steps_controller.rb
@@ -1,11 +1,17 @@
     class Api::V1::GradingScaleStepsController < Api::V1::BaseController
 
+      # Description of #create
+      # @return [GradingScaleStep] returns created GradingScaleStep object
+      # @author Divyanshu
       def create
         @grading_scale_step = GradingScaleStep.create(grading_scale_step_params)
         render_error(@grading_scale_step.errors.full_messages) and return if @grading_scale_step.errors.present?
         render_object(@grading_scale_step, { name: 'grading_scale_step' }, {})
       end
 
+      # Description of #destroy
+      # @return [String] Success Message on successful deletion
+      # @author Divyanshu
       def destroy
         @grading_scale_step = GradingScaleStep.find(params[:id])
         @grading_scale_step.destroy
@@ -13,11 +19,17 @@
         render_success
       end
 
+      # Description of #show
+      # @return [GradingScaleStep] grading scale steps details
+      # @author Divyanshu
       def show
         @grading_scale_step = GradingScaleStep.find(params[:id])
         render_object(@grading_scale_step, { name: 'grading_scale_step' }, {})
       end
 
+      # Description of #update
+      # @return [GradingScaleStep] updated GradingScaleStep object
+      # @author Divyanshu
       def update
         @grading_scale_step = GradingScaleStep.find(params[:id])
         @grading_scale_step.update_attributes(grading_scale_step_params)
@@ -25,6 +37,9 @@
         render_object(@grading_scale_step, { name: 'grading_scale_step' }, {})
       end
 
+      # Description of #restore
+      # @return [GradingScaleStep] restored GradingScaleStep object
+      # @author Divyanshu
       def restore
         @grading_scale_step = GradingScaleStep.restore(params[:id])
         render_object(@grading_scale_step, { name: 'grading_scale_step' }, {})
